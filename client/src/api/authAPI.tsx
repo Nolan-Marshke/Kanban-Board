8f0c22746c48d0597eb53a4116a19cb2de2e04d9
Nolan Marshke <nmarshke@gmail.com>
2025-02-03 23:48:49 -0500

    added authAPI

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,29 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
 
+    if (!response.ok) {
+      throw new Error("Failed to log in");
+    }
 
+    const data = await response.json();
+    
+    const token = data.token;
+    
+    localStorage.setItem("authToken", token);
+    console.log("Login successful");
+    return token;
+  } catch (error) {
+    console.error("Login error:", error);
+  }
+};
 
 export { login };
