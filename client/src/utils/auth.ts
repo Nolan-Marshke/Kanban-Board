e948c79f268a0de89fff18410ec7dce9ea1689c0
nolan MARSHKE <nmarshke@gmail.com>
2025-03-23 11:40:06 -0400

    finished getProfile in utils

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -2,30 +2,36 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    const token = this.getToken();
+    if (!token) return null;
+    
+    try {
+      return jwtDecode(token) as JwtPayload;
+    } catch (error) {
+      console.error('Invalid token', error);
+      return null;
+    }
+  }
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    
   }
   
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    
   }
 
   getToken(): string {
-    // TODO: return the token
+    
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    e
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
-  }
+    
 }
 
 export default new AuthService();
