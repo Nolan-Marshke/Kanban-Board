81f5bc6c65d1b6fe8f6d99586be72dc065d760e9
nolan MARSHKE <nmarshke@gmail.com>
2025-03-23 11:24:21 -0400

    added code to auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,22 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+
+  export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if (!token) {
+      return res.sendStatus(401),json({ message: 'Unauthorized' });
+    }
+
+    try {
+      const secret = process.env.JWT_SECRET || '';
+      const decoded = jwt.verify(token, secret) as JwtPayload;
+      req.user = { username: decoded.username };
+      next();
+
+    } catch (error) {
+      return res.sendStatus(403).json({ message: 'Forbidden' });
+    }
 };
