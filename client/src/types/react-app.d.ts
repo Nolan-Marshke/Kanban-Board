3672b12b64f3e86b1dd245c80297440bb3e64526
nolan MARSHKE <nmarshke@gmail.com>
2025-03-23 12:43:56 -0400

    fixed render errors

--- client/src/types/react-app.d.ts
+++ client/src/types/react-app.d.ts
@@ -1,15 +1,15 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
+declare module 'react' {
+  export = React;
+  export as namespace React;
+}
 
 declare module 'react/jsx-runtime' {
-    export default JSX.Element;
-    export const jsx: (type: React.ElementType, props: Record<string, unknown>) => JSX.Element;
-    export const jsxs: (type: React.ElementType, props: Record<string, unknown>) => JSX.Element;
-    export const Fragment: React.FC;
-  }
-  
-  declare namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-  }
+  export const jsx: any;
+  export const jsxs: any;
+  export const Fragment: any;
+}
+
+declare module 'react-dom' {
+  export = ReactDOM;
+  export as namespace ReactDOM;
+}
